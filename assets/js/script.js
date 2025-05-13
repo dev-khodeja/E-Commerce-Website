3cedb1173c3ff60163da0adf5669aff871f5dfe1
khodeja Aktar <khodejaaktar713@gmail.com>
2025-05-13 22:48:27 +0600

    E-Commerce

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// Date Funtction
+const date=document.getElementById("date").innerHTML=new Date();
+
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
